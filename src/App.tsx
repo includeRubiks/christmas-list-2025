a9f34f750751a8de9cdfcef513a9704efc71548e
Doug Mead <fremonade@gmail.com>
2025-11-14 16:24:28 -0500

    Automated commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import ListItem from "./components/Item.tsx"
 
 function App() {
   return (
-    <div className="text-center" data-bs-theme="dark">
+    <div className="text-center bg-dark text-light">
       <h1>Doug's christmas list</h1>
       <ol>
         <ListItem name="Filament!" url="https://www.amazon.com/dp/B0991P3Q25"/>
