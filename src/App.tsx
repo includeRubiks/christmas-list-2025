89f726266f64a4ddab70ba347b39b453db0ac755
Doug Mead <fremonade@gmail.com>
2025-11-14 16:15:59 -0500

    Automated commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Doug's christmas list</h1>
       <ol>
         <ListItem name="Filament!" url="https://www.amazon.com/dp/B0991P3Q25"/>
+        <ListItem name="The newest Raspberry Pi Zero" url="https://www.digikey.com/en/products/detail/raspberry-pi/SC1176/15298147"/>
         <ListItem name="Nozzles for my Ender 3!" url="https://www.amazon.com/Creality-Nozzles-3D-Printer-Extruder/dp/B093SKXHL3"/>
         <ListItem name="Thing so my printer can print multicolor!" url="https://www.microcenter.com/product/685299/bambu-lab-ams-lite"/>
         <ListItem name="Tiny computer!" url="https://www.digikey.com/en/products/detail/m5stack-technology-co-ltd/K132-V11/26267927"/>
